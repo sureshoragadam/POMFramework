5a0b529db4ff5072f87360ad3cd835b1c9674df6
sureshoragadam <44584042+sureshoragadam@users.noreply.github.com>
2018-10-30 17:12:26 +0530

    changes in pom file

--- src/test/java/NewTestpom1.java
+++ src/test/java/NewTestpom1.java
@@ -1,8 +1,6 @@
 package test.java;
 
 import org.testng.annotations.Test;
-
-
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
